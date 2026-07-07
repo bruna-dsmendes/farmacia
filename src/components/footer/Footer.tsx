ffeac14fd9b0f2157a817f17aca24c9e7fea38cd
Bruna Dos Santos Mendes <brunadsantos.mendes@gmail.com>
2026-07-07 16:39:33 -0300

    refactor: complete project restructure to match Blog_pessoal_react architecture

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className='bg-blue-600 text-white text-center py-4 mt-8'>
+      <p>&copy; 2026 Farmácia. Todos os direitos reservados.</p>
+    </footer>
+  )
+}
+
+export default Footer
